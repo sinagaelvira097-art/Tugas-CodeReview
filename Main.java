c84aa71e954543c9ba5ad2bd2dd453155c3804a3
sinagaelvira097-art <sinagaelvira097@gmail.com>
2026-06-14 15:32:57 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Program sederhana penjumlahan
+
         int a = 10;
         int b = 20;
 
         System.out.println(a+b);
         System.out.println(a+b);
         System.out.println(a+b);
-
     }
 }
